580637a7d74ee3ea4c540560517082f30893e5d7
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-05-19 15:19:14 +0300

    Improved fixtures

--- docker/migrations/docker-entrypoint.sh
+++ docker/migrations/docker-entrypoint.sh
@@ -21,5 +21,13 @@ done
 php bin/console doctrine:migrations:migrate --no-interaction
 
 if [ "$LOAD_FIXTURES" = "1" ]; then
+    # Replace localhost with BASE_DOMAIN in fixtures.yml
+    if [ -z "$BASE_DOMAIN" ]; then
+        sed -i "s/localhost/$BASE_DOMAIN/g" vendor/sylius/sylius/src/Sylius/Bundle/CoreBundle/Resources/config/app/fixtures.yml
+    fi
+
     php bin/console sylius:fixtures:load --no-interaction
+
+    # generate image cache
+    find public/media/image -type f -print0 | sed 's/public\/media\/image\///' | xargs -0 -I{} sh -c 'bin/console liip:imagine:cache:resolve {} || true'
 fi
